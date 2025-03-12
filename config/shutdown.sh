4cf2af764e4a700508f398d70c48ae0f43cdeb30
msabry2020 <69658996+msabry2020@users.noreply.github.com>
2025-03-12 15:02:49 +0200

    short version of shutdown.sh

--- config/shutdown.sh
+++ config/shutdown.sh
@@ -3,52 +3,16 @@
 # Graceful Cluster Shutdown Script for OpenShift Container Platform 4.17
 # Reference: https://docs.openshift.com/container-platform/4.17/backup_and_restore/graceful-cluster-shutdown.html
 
-# Variables
-MASTER_NODES=("master-0" "master-1" "master-2")  # Replace with your master node hostnames
-WORKER_NODES=("worker-0" "worker-1" "odf-0" "odf-1" "odf-2")  # Replace with your worker node hostnames
-SSH_KEY="/home/eng_muhammedsabry/.ssh/id_rsa"  # Replace with the path to your SSH private key
+# Step 1: Mark all the nodes in the cluster as unschedulable
+echo "cordon nodes..."
+for node in $(oc get nodes -o jsonpath='{.items[*].metadata.name}'); do echo ${node} ; oc adm cordon ${node} ; done
 
-# Function to check if a command was successful
-check_success() {
-    if [ $? -ne 0 ]; then
-        echo "Error: $1 failed."
-        exit 1
-    fi
-}
+# Step 2: Evacuate the pods using the following method:
+echo "drain nodes..."
+for node in $(oc get nodes -l node-role.kubernetes.io/worker -o jsonpath='{.items[*].metadata.name}'); do echo ${node} ; oc adm drain ${node} --delete-emptydir-data --ignore-daemonsets=true --timeout=15s --force ; done
 
-# Step 1: Drain worker nodes
-echo "Draining worker nodes..."
-for NODE in "${WORKER_NODES[@]}"; do
-    oc adm drain "$NODE" --ignore-daemonsets --delete-emptydir-data --force --timeout=15s #--disable-eviction 
-    check_success "Draining node $NODE"
-done
-
-# Step 2: Stop kubelet on worker nodes
-echo "Stopping kubelet on worker nodes..."
-for NODE in "${WORKER_NODES[@]}"; do
-    ssh -i "$SSH_KEY" "core@$NODE" "sudo systemctl stop kubelet"
-    check_success "Stopping kubelet on $NODE"
-done
-
-# Step 3: Stop kubelet on master nodes
-echo "Stopping kubelet on master nodes..."
-for NODE in "${MASTER_NODES[@]}"; do
-    ssh -i "$SSH_KEY" "core@$NODE" "sudo systemctl stop kubelet"
-    check_success "Stopping kubelet on $NODE"
-done
-
-# Step 4: Power off master nodes
-echo "Powering off master nodes..."
-for NODE in "${MASTER_NODES[@]}"; do
-    ssh -i "$SSH_KEY" "core@$NODE" "sudo shutdown -h now"
-    check_success "Powering off $NODE"
-done
-
-# Step 5: Power off worker nodes
-echo "Powering off worker nodes..."
-for NODE in "${WORKER_NODES[@]}"; do
-    ssh -i "$SSH_KEY" "core@$NODE" "sudo shutdown -h now"
-    check_success "Powering off $NODE"
-done
+# Step 5: Power off  nodes
+echo "Powering off nodes..."
+for node in $(oc get nodes -o jsonpath='{.items[*].metadata.name}'); do oc debug node/${node} -- chroot /host shutdown -h 1; done 
 
 echo "Graceful cluster shutdown completed successfully."
